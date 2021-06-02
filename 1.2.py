83454a489af3ed63c221edd8b08cdfbdeb4ae76b
seanpark0603 <39549580+seanpark0603@users.noreply.github.com>
2021-06-02 10:54:41 +0900

    Update 1.2.py

--- 1.2.py
+++ 1.2.py
@@ -1,5 +1,6 @@
 dna = ' gcatgacgttattacgactctgtcacgccgcggtgcgactgaggcgtggcgtctgctgggcctttacttcgcctccgcgccctgcattccgttcctggcctcg '
 dna = dna.replace(" ", "")
+dna = dna.replace("a", "A")
 dna = dna.replace("t", "a")
-dna = dna.replace("a", "t")
+dna = dna.replace("A", "t")
 percent = (dna.count("gc")/len(dna))*100
